45d845c3ddca175c4635cce6e8c449667fec4497
moin2672 <moin2672@gmail.com>
2023-01-06 09:00:36 +0530

    4. useReducer() Hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,85 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (prevState, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+  }
+  if (action.type === 'INPUT_BLUR') {
+    return { value: prevState.value, isValid: prevState.value.includes('@') };
+  }
+  return { value: '', isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-/*
-useEffect(()=>{
-  console.log('RUNNING EFFECT')
-})
+  /*
+  SYNTAX:  
+  const [state, dispatchFn] = useReducer(reducerFn, initialState, initFn);
 
-runs when the component FIRST mounts & also for EVERY STATE UPDATE
+  reducerFn = (prevState, action) => newState
 
-useEffect(()=>{
-  console.log('RUNNING EFFECT')
-},[])
+  NOTE: 
+  1. initialState & prevState - will be of same type
+  2. action - used to {type:'', val:...} 
+  3. reducerFn - sets / modify State
+  4. dispatchFn - sets / modify action
 
-runs ONLY ONCE when the component FIRST mounted
+  WORKFLOW:
+  const [state, dispatchFn] --> workflow below
+  state => initialState
+  dispatchFn --> {type:'', val:...} (action) --> reducerFn --> (stateChanges) prevState ==> can be used in Fnlogics/JSX
 
-useEffect(()=>{
-  console.log('RUNNING EFFECT')
-},[enteredPassword])
-
-runs when the component FIRST mounts & whenever was reevaluated or whenever the enteredPassword changes it runs
-
-seEffect(()=>{
-  console.log('RUNNING EFFECT')
-
-  return ()=>{
-    console.log('EFFECT CLEANUP')
-  }
-},[enteredPassword])
-
-if it has depedency like enteredPassword, then RUNNING EFFECT runs at the begining and 
-EFFECT CLEANUP runs before the RUNNING EFFECT, whenever the enteredPassword changes.
-
-useEffect(()=>{
-  console.log('RUNNING EFFECT')
-
-  return ()=>{
-    console.log('EFFECT CLEANUP')
-  }
-},[])
-
-without dependency, the RUNNING EFFECT runs at the begining ONCE &
-EFFECT CLEANUP runs at the end when the component is removed, i.e. when we moved from LOGIN to HOME component
-
-*/
-
-
+  NOTE2:
+  emailReducer can be declated/used outside of Login Fn component
+  */
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  });
 
   useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log('Checking form validity!');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500);
+    console.log('EFFECT RUNNING');
 
     return () => {
-      console.log('CLEANUP');
-      clearTimeout(identifier);
+      console.log('EFFECT CLEANUP');
     };
-  }, [enteredEmail, enteredPassword]);
+  }, []);
+
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log('Checking form validity!');
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+
+  //   return () => {
+  //     console.log('CLEANUP');
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value});
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({type: 'INPUT_BLUR'});
   };
 
   const validatePasswordHandler = () => {
@@ -88,7 +94,7 @@ EFFECT CLEANUP runs at the end when the component is removed, i.e. when we moved
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -96,14 +102,14 @@ EFFECT CLEANUP runs at the end when the component is removed, i.e. when we moved
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
