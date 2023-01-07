91c06262499bb5b0e039d3c3a419ad889248ad6d
moin2672 <moin2672@gmail.com>
2023-01-07 09:14:22 +0530

    8. making context dynamic (using functions in context)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Card from '../UI/Card/Card';
 import Button from '../UI/Button/Button';
 import classes from './Home.module.css';
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -3,9 +3,9 @@ import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
-console.log("ctx=",ctx);
+
   return (
     <nav className={classes.nav}>
       <ul>
@@ -21,7 +21,7 @@ console.log("ctx=",ctx);
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
